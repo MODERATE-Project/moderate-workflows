3d21d2b3db81c05f7dcf03fedc9e3262ce716a1a
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2023-09-22 16:40:50 +0200

    🐛 Fix Open Metadata secret generation script

--- scripts/update-open-metadata-secret.sh
+++ scripts/update-open-metadata-secret.sh
@@ -7,13 +7,15 @@ tea +yq
 
 temp_folder=$(python3 -c 'import tempfile; print(tempfile.gettempdir())')
 temp_secrets=$temp_folder/dagster-secrets.yaml
+temp_token=$temp_folder/om-token
 
 kubectl get secret ${SECRET_NAME} -n ${NAMESPACE} -o yaml >$temp_secrets
-value=${OPEN_METADATA_TOKEN}
-value_base64=$(echo -n "$value" | base64)
+printf '%s' "$OPEN_METADATA_TOKEN" >$temp_token
+value_base64=$(base64 -i $temp_token)
 yq e ".data.OPEN_METADATA_TOKEN = \"$value_base64\"" -i $temp_secrets
 kubectl apply -f $temp_secrets
 rm $temp_secrets
+rm $temp_token
 
 # We need to patch the deployment to force the secret to be updated
-kubectl rollout restart deployment ${DEPLOYMENT_NAME} -n ${NAMESPACE}
+helm upgrade ${DAGSTER_RELEASE} ${DAGSTER_CHART} --recreate-pods
