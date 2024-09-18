9ee87d214daa374da95ae3d858e716cf579d53a2
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2024-09-18 10:42:24 +0200

    📌 Pin numpy to <2

--- moderate/setup.py
+++ moderate/setup.py
@@ -12,6 +12,7 @@ setup(
         f"dagster-postgres=={_VERSION_DAGSTER_EXT}",
         f"dagster-k8s=={_VERSION_DAGSTER_EXT}",
         "pandas[performance,postgresql]>=2.0,<3.0",
+        "numpy>=1.0,<2.0",
         "python-keycloak>=2.16.1,<3.0",
         "sh==2.0.6",
         "openmetadata-ingestion[postgres,datalake-s3,datalake-gcp]==1.3.1",
